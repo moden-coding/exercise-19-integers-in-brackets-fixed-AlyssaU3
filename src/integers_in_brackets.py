6f01ccb3d200f6eeaea08cebfc56e0687eb9237b
AlyssaU3 <alys4321c@gmail.com>
2025-09-17 13:09:47 -0400

    19/21 done

--- src/integers_in_brackets.py
+++ src/integers_in_brackets.py
@@ -2,7 +2,17 @@
 import re
 
 def integers_in_brackets(s):
-    pass
+    test1=(re.findall(r"\[\s*?([-]?[^a]?\d\d)\s?\]", s))
+    test2= list(map(int, test1))
+    #thing1=[]
+    #for thing in test1:
+        #thing.strip("+")
+    #print(thing)
+    #thing1.append(thing)
+        #print_out=[]
+        #print_out.append(s)
+    return(test2)
+    #return test1
     
     
 
@@ -10,10 +20,10 @@ def main():
     print(integers_in_brackets(" afd [asd] [12 ] [a34] [ -43 ]tt [+12]xxx")) 
     #Output for the above should be: [12, -43, 12]
 
-    print(integers_in_brackets("  afd [128+] [47 ] [a34]  [ +-43 ]tt [+12]xxx")
+    print(integers_in_brackets("  afd [128+] [47 ] [a34]  [ +-43 ]tt [+12]xxx"))
     #Output for the above should be: [47, 12]
     
-    print(integers_in_brackets("")
+    print(integers_in_brackets(""))
     #Output for the above should be: []
     
     
